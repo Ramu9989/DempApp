9d565b12cbf3e382e75f77a80b313a0284aa9865
Ramu9989 <bandari.ramu5@gmail.com>
2020-11-07 12:19:33 +0530

    initial commit

---
+++ DemoApp/SaveData.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SaveData.aspx.cs" Inherits="DemoApp.SaveData" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div  style="margin-left: 500px;" >
+  <asp:label runat="server" text="Enter Values Here to Save data:" Font-Bold="true" ForeColor="Blue"></asp:label><br />
+
+        <asp:label runat="server" text="Fisrt Name  :"></asp:label>
+        <asp:TextBox ID="txtFistName" runat="server"></asp:TextBox><br />
+
+        <asp:label runat="server" text="Last Name:"></asp:label>
+        <asp:TextBox ID="txtLastName" runat="server" style="margin-left: 6px;"></asp:TextBox><br />
+
+        
+        <asp:label runat="server" text="Gender    :"></asp:label>
+        <%--<asp:TextBox ID="txtGender" runat="server" style="margin-left: 20px;"></asp:TextBox>--%>
+        <asp:DropDownList ID="ddGender" runat="server"  style="margin-left: 24px;" >  
+               
+            <asp:ListItem>Male</asp:ListItem> 
+            <asp:ListItem>Female </asp:ListItem>  
+            <asp:ListItem>Transgender</asp:ListItem>                 
+        </asp:DropDownList>  
+        <br />
+
+         
+        <asp:label runat="server" text="Salary:"></asp:label>
+        <asp:TextBox ID="txtSalary" runat="server" style="margin-left: 35px;"></asp:TextBox><br />
+        
+          
+        
+        
+         <asp:label runat="server" text="Search:"></asp:label>
+        <asp:TextBox ID="txtSearch" runat="server" style="margin-left: 32px;"></asp:TextBox><br />
+         <asp:Button ID="btnSave" runat="server" Text="Save" onclick="btnSave_Click" style=" margin-left: 88px;"/>
+        <asp:Button ID="btnGetData" runat="server" Text="Get Data" onclick="btnGetData_Click" style=" margin-left: 51px;"/><br /> 
+        
+
+
+        <asp:label ID="lblMsg"   runat="server" text="" Font-Bold="true"></asp:label><br />
+         <asp:label ID="lblLastName"   runat="server" text="" Font-Bold="true"></asp:label><br />
+         <asp:label ID="lblSalay"   runat="server" text="" Font-Bold="true"></asp:label>
+       
+<%--         <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+Style="top: 98px; left: 367px; position: absolute; height: 26px; width: 162px" 
+ErrorMessage="Fisrt name required" ControlToValidate="txtFistName" ForeColor="Red">
+</asp:RequiredFieldValidator>  
+               <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" 
+Style="top: 98px; left: 500px; position: absolute; height: 26px; width: 190px" 
+ErrorMessage="Last name required" ControlToValidate="txtLastName" ForeColor="Red" >
+</asp:RequiredFieldValidator>  
+                       <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" 
+Style="top: 98px; left: 700px; position: absolute; height: 26px; width: 210px" 
+ErrorMessage="Gender  required" ControlToValidate="txtGender" ForeColor="Red" >
+</asp:RequiredFieldValidator> 
+                               <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" 
+Style="top: 98px; left: 900px; position: absolute; height: 26px; width: 210px" 
+ErrorMessage="Salary  required:" ControlToValidate="txtSalary" ForeColor="Red" >
+</asp:RequiredFieldValidator> --%>
+
+        <%--<asp:GridView ID="GridView1" runat="server"></asp:GridView>--%>
+         <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+        </asp:GridView>
+
+    </div>
+        
+    </form>
+</body>
+</html>
+
---
+++ DemoApp/SaveData.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace DemoApp
+{
+    public partial class SaveData : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+              
+                ddGender.DataBind();
+            }
+        }
+        protected void btnSave_Click(object sender, EventArgs e)
+
+        {
+
+            string connetionString;
+            SqlConnection cnn;
+
+            connetionString = @"Data Source=RISHIKA-PC;Initial Catalog=EmployeeDB ;User ID=RAMU;Password=rishikabb";
+            //connetionString = @"Data Source=Localhost;Initial Catalog=EmployeeDB ;User ID=RAMU;Password=rishikabb";
+            cnn = new SqlConnection(connetionString);
+
+            cnn.Open();
+
+            //Response.Write("Connection made");
+
+            //SqlCommand cmd = new SqlCommand("insert into  Employees (FirstName,LastName)    values('" + txtFistName.Text + "','" + txtLastName.Text + "')", cnn);
+
+            if (!Regex.IsMatch(txtSalary.Text,@"(^([0-9]*|\d*\d{1}?\d*)$)"))
+            {
+                lblMsg.Text = "Please enter only numbers  in Salary field";
+            }
+            else
+            {
+
+                if (txtFistName.Text == "")
+                {
+                    lblMsg.Text = "Please Enter First Name ";
+                }
+                else
+                {
+                    if (txtLastName.Text == "")
+                    {
+                        lblMsg.Text = "Please Enter Last Name ";
+                    }
+                    else
+                    {
+                        if (txtSalary.Text == "")
+                        {
+                            lblMsg.Text = "Please Enter Salary ";
+                        }
+                        else
+                        {
+                            SqlCommand cmd = new SqlCommand("insert into  Employees (FirstName,LastName,Gender,salary)    values('" + txtFistName.Text + "','" + txtLastName.Text + "' ,'" + ddGender.SelectedValue + "' ," + txtSalary.Text + ")", cnn);
+                            cmd.ExecuteNonQuery();
+                            lblMsg.Text = "Your data inserted successfully";
+                        }                       
+                    }                   
+                }
+
+            }
+            cnn.Close();
+            lblMsg.Visible = true;
+            lblMsg.ForeColor = System.Drawing.Color.Red;
+          
+            txtFistName.Text = "";
+            txtLastName.Text = "";
+            txtSalary.Text = "";
+            GridView1.Visible = false;
+            //txtGender.Text = "";
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnGetData_Click(object sender, EventArgs e)
+        {
+
+            lblMsg.Text = "";
+            GridView1.Visible = true;
+            string connetionString;
+            SqlConnection cnn;
+            SqlDataAdapter da = null;
+            DataSet ds = null;
+
+            connetionString = @"Data Source=RISHIKA-PC;Initial Catalog=EmployeeDB ;User ID=RAMU;Password=rishikabb";
+
+            cnn = new SqlConnection(connetionString);
+
+            cnn.Open();
+
+            if (txtSearch.Text == "")
+            {
+               da = new SqlDataAdapter("Select * From Employees ", cnn);
+            }
+            else
+          
+            {
+                if (!Regex.IsMatch(txtSearch.Text, @"(^([0-9]*|\d*\d{1}?\d*)$)"))
+                {
+
+                    da = new SqlDataAdapter("Select * From Employees where FirstName=@FirstName or LastName=@LastName or Gender=@Gender ", cnn);
+
+                    da.SelectCommand.Parameters.AddWithValue("@FirstName", txtSearch.Text);
+                    da.SelectCommand.Parameters.AddWithValue("@LastName", txtSearch.Text);
+                    da.SelectCommand.Parameters.AddWithValue("@Gender", txtSearch.Text);
+
+                }
+                else
+                {
+                    da = new SqlDataAdapter("Select * From Employees where Salary=@Salary ", cnn);
+                    da.SelectCommand.Parameters.AddWithValue("@Salary", txtSearch.Text);
+                }
+            }
+
+            //if (!Regex.IsMatch(txtSalary.Text, @"(^([0-9]*|\d*\d{1}?\d*)$)"))
+            //{
+            //    da.SelectCommand.Parameters.AddWithValue("@Salary", txtSearch.Text);
+            //}
+         
+         
+            ds = new DataSet();
+            da.Fill(ds, "EmployeeDetails");
+            GridView1.DataSource = ds.Tables["EmployeeDetails"];
+            GridView1.DataBind();
+            cnn.Close();
+
+        }
+
+
+    }
+}
